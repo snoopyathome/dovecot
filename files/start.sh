67f6a5e1c773212beb0319f93748416ae04c3726
snoopyathome <mail-an-muehlbauer@web.de>
2019-12-04 15:24:08 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rsyslogd
 cp -R /defaults/* /etc/dovecot/
+addgroup vmail
 adduser -G vmail -h /vmail -H -D vmail
 /usr/sbin/dovecot -c /etc/dovecot/dovecot.conf
 tail -F /var/log/dovecot.log
